f4d6e7dd0049fbc5a2e60b154ee355028199a060
Armaan A <armaanaggarwal@gmail.com>
2022-12-23 22:34:05 -0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Thing = () => {
+  return (
+    // We are adding these classes here
+    <div className="mt-10 bg-red-100 p-5 rounded-md">
+      {`the snozzberries taste like snozzberries`}
+    </div>
+  );
+};
