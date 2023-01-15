24bc66203137e399bf4e73eace8baef7f65c1537
Armaan A <armaanaggarwal@gmail.com>
2023-01-14 21:26:51 -0800

    add some more outlined buttons

--- src/Button.tsx
+++ src/Button.tsx
@@ -27,17 +27,26 @@ const solidClasses = {
   ),
   black: cn(
     "bg-black hover:bg-black/70 active:bg-black/70 ring-neutral-400 text-white",
-    "dark:bg-white dark:text-black"
+    "dark:bg-white dark:hover:bg-gray-300 dark:active:bg-gray-300 dark:text-black"
   ),
 };
 
 const outlineClasses = {
-  red: "",
-  green: "",
-  blue: "",
+  red: cn(
+    "border-red-600 text-red-600 hover:bg-red-100 active:bg-red-100 ring-red-700/50 focus:border-red-700/80",
+    "dark:border-red-400 dark:text-red-400 dark:hover:bg-red-900/80 dark:active:bg-red-900/80 dark:ring-red-600 dark:focus:border-red-500"
+  ),
+  green: cn(
+    "border-emerald-600 text-emerald-700 hover:bg-emerald-100 active:bg-emerald-100 ring-emerald-700/50 focus:border-emerald-700/80",
+    "dark:border-emerald-400 dark:text-emerald-400 dark:hover:bg-emerald-900/80 dark:active:bg-emerald-900/80 dark:ring-emerald-600 dark:focus:border-emerald-600"
+  ),
+  blue: cn(
+    "border-blue-600 text-blue-600 hover:bg-blue-100 active:bg-blue-100 ring-blue-600/50 focus:border-blue-600",
+    "dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-900/80 dark:active:bg-blue-900/80 dark:ring-blue-600 dark:focus:border-blue-600"
+  ),
   gray: cn(
     "border-gray-400 text-gray-500 hover:bg-gray-200 active:bg-gray-200 ring-gray-500/80 focus:border-gray-500/80",
-    "dark:border-gray-400 dark:text-gray-300 dark:hover:bg-gray-600 dark:active:bg-gray-600 dark:ring-white/50 dark:focus:border-white/50"
+    "dark:border-gray-400 dark:text-gray-300 dark:hover:bg-gray-600 dark:active:bg-gray-600 dark:ring-white/70 dark:focus:border-white/70"
   ),
   black: ""
 }
@@ -56,7 +65,7 @@ export const Button = ({
         "duration-150 font-medium rounded border-2",
         // Focus ring
         "focus-visible:outline-2 outline-offset-4",
-        variant === "solid" ? "not-focus-visible:focus:ring-[2.5px]" : "not-focus-visible:focus:ring-1",
+        variant === "solid" ? "not-focus-visible:focus:ring-[3px]" : "not-focus-visible:focus:ring-1",
         // Size
         size === "small"
           ? "px-3 py-2 text-sm"
